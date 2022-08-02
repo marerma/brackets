f4e104426e23f882840956ee926c51c532564598
Maria Ermolaeva <maria.kadantseva@gmail.com>
2022-08-02 13:55:52 +0300

    feat: solved brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let stack = []
+    
+    const brDic = bracketsConfig.flat().reduce((a, b, i, array) => {
+    if (i % 2 === 0) {
+      a[b] = array[i+1]
+    } 
+    return a
+    }, {})
+    
+    
+    for (let i = 0; i < str.length; i++) {
+      currentBracket = str[i]
+      let topEl = stack[stack.length - 1]
+      console.log('i =', i)
+      console.log('current =', currentBracket)
+      console.log('top =', topEl)
+    
+      console.log(brDic[topEl] === currentBracket && stack.length != 0)
+    
+      if (brDic[topEl] === currentBracket && stack.length != 0) {
+        stack.pop()
+        console.log('stack', stack)
+      }
+      else {
+        stack.push(currentBracket)
+        console.log('stack', stack)
+      }
+     
+    }
+    return stack.length === 0
+    }
+    
